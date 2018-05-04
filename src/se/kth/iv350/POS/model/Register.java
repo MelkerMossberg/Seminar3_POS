ca5dff963f7369426434c44d005401feb74e43f7
Sneriko <erik.lenas@gmail.com>
2018-05-04 11:14:42 +0200

    More comments from Erik

--- src/se/kth/iv350/POS/model/Register.java
+++ src/se/kth/iv350/POS/model/Register.java
@@ -9,6 +9,14 @@ public class Register {
     public Register(){
     }
 
+    /**
+     * Creates an instance of <code>Receipt</code> and calls the function <code>PrintReceipt</code> in
+     * <code>ReceiptPrinter</code>.
+     * @param purchaseDTO The <code>PurchaseDTO</code> with which the function creates the receipt.
+     * @param amountPayed The amount payed by the customer.
+     * @param change The change given to the customer.
+     */
+
     public void getReceipt(PurchaseDTO purchaseDTO, int amountPayed, int change){
         receipt = new Receipt(purchaseDTO, amountPayed, change);
         receiptPrinter.PrintReceipt(receipt);
